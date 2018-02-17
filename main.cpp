0fe2aa344aa7db5a274ac7591b6af3c78d7aa559
HeoCRAFTrus <33593229+HeoCRAFTrus@users.noreply.github.com>
2018-02-17 18:07:48 +0300

    работа с кодом
    -работа над рикошетом...
    -созданы объекты-повреждённые части танков

--- main.cpp
+++ main.cpp
@@ -1274,7 +1274,31 @@ class MyApp : public App
 		{
 			auto vecsh = Vec2(2000.0, 0.0);
 			vecsh.rotate(shell.angle());
+            shaders2.data(shell) = shell.pos();
 			shell.setPos(shell.pos() + vecsh / stepn * timeDelta());
+            //////////////////////////////////////проверка на рикошет, непробитие и попадание в гусеницу///////////
+            float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+            x2 = shaders2.data(shell).x;
+            y2 = shaders2.data(shell).y;
+            x1 = shell.pos().x;
+            y1 = shell.pos().y;
+            k1 =(y2-y1)/(x2-x1);
+            b1=y1-(k1*x1);
+            Vec2 p3(32, 16);
+            p3.rotate(mainhs.angle());
+            p3+= mainhs.pos();
+            x3 =p3.x;
+            y3 = p3.y;
+            Vec2 p4(-32, 16);
+            p4.rotate(mainhs.angle());
+            p4 += mainhs.pos();
+            x4 = p4.x;
+            y4 = p4.y;
+            k2 = (y4 - y3) / (x4 - x3);
+            b2 = y3 - (k2*x3);
+            float x5 = 0, y5 = 0;
+            x5 = (b1-b2)/(k2-k1);
+            y5 = k1+b1;
 			if (!shell.box().intersects(gamepole.gameBox()))
 			{
 				shaders2.remove(shell);
@@ -1580,7 +1604,7 @@ class MyApp : public App
     LayerFromDesign(void, shaders1);
     LayerFromDesign(void, traki);
     LayerFromDesign(Vec2, dust);
-	LayerFromDesign(void, shaders2);
+	LayerFromDesign(Vec2, shaders2);
 	LayerFromDesign(void, guns);
 	LayerFromDesign(void, towers);
 	FromDesign(GameObj, rload);
@@ -1658,6 +1682,9 @@ class MyApp : public App
 	FromDesign(Layout, stolb1);
 	LayerFromDesign(void, graund);
     LayerFromDesign(void, clay);
+
+    float k1=0, k2=0, b1=0, b2=0;
+    float x3=0,x4=0,y3=0,y4=0;
 
 	vector<gundata> gddos;
 	vector<tdata> tddos;
